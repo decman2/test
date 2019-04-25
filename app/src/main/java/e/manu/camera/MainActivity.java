dadde28f1de87ce41bb2e0ea9f595df04c714b06
Manuel De Ceglia <manueldeceglia@smc-essen.de>
2019-04-25 21:11:23 +0200

    Third

--- app/src/main/java/e/manu/camera/MainActivity.java
+++ app/src/main/java/e/manu/camera/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("SECOND BRANCH");
 //    }
 //        cameraView = findViewById(R.id.camera);
 //        cameraView.addCameraListener(new CameraListener() {
