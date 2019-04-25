b620eda68c254fc8439dc51123f36dd123f178e5
Manuel De Ceglia <manueldeceglia@smc-essen.de>
2019-04-25 21:03:56 +0200

    First Master Commit

--- app/src/main/java/e/manu/camera/MainActivity.java
+++ app/src/main/java/e/manu/camera/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("MASTER");
 //    }
 //        cameraView = findViewById(R.id.camera);
 //        cameraView.addCameraListener(new CameraListener() {
